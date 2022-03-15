f9e4ac77857f11225c7d3ae6d0a4afca0a57bdae
Tal Zaken <talzaken@gmail.com>
2022-03-15 16:15:30 -0400

    Added model for storing extracted colors in DB

--- models.py
+++ models.py
@@ -18,3 +18,12 @@ class User(Model):
 
 User_Pydantic = pydantic_model_creator(User, name='User')
 UserIn_Pydantic = pydantic_model_creator(User, name='UserIn', exclude_readonly=True)
+
+
+class Color(Model):
+    #user = fields.ForeignKeyField()
+    name = fields.CharField(50)
+    url = fields.CharField(1000)
+    num_colors = fields.IntField()
+    dominant_colors = fields.CharField(1000)
+    avg_inertia = fields.FloatField()
